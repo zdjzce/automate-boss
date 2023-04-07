ea40a8387631bed63cce49bea8bae5051f09d26d
zdjce923 <493904954@qq.com>
2023-04-08 03:55:32 +0800

    feat: 放弃login滑块验证

--- main.py
+++ main.py
@@ -18,19 +18,24 @@ def runWebdriver():
     driver.set_window_size(414, 896)
     driver.implicitly_wait(4)
     driver.get('https://www.zhipin.com/beijing/')
-    initLogin(driver=driver)
+    # initLogin(driver=driver)
 
-
+# TODO login的滑块验证开发成本太高
 def initLogin(driver):
     driver.find_element(
         By.XPATH, '//*[@id="wrap"]/div[1]/div[2]/a/div').click()
     time.sleep(3)
 
+    driver.find_element(By.XPATH, '//*[@id="regVerrifyCode"]/div').click()
+    time.sleep(5)
+
     driver.find_element(
         By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[3]/span[2]/input').send_keys('13522142948')
-    time.sleep(1)
+    time.sleep(2)
 
-    driver.find_element(By.XPATH, '//*[@id="regVerrifyCode"]/div').click()
+    driver.find_element(By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[5]/span/button').click()
+
+    driver.find_element(By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[7]/input').click()
 
 
 if __name__ == '__main__':

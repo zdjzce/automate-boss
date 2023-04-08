668031bdb327f837a0f95617e6b19cc37aeb3f1e
zdjce923 <493904954@qq.com>
2023-04-08 16:20:24 +0800

    feat: auto scroll bottom

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
 
-def getOptions():
+def get_options():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('detach', True)  # 不自动关闭浏览器
     options.add_experimental_option(
         'mobileEmulation', {'deviceName': 'iPhone XR'})
-    options.add_argument('--start-maximized')  # 浏览器窗口最大化
+    # 设置 user-data-dir chrome会加载此配置 里面包含了用户的 cookie
+    # 如果自定义的user-data-dir文件夹 登录获取验证会失败 暂时使用第一次启动的文件夹 通过 chrome://version 查看目录 保持登录状态
+    options.add_argument("user-data-dir=C:\\Program Files (x86)\\scoped_dir15408_969916617")
+    # 获取本地提前启动好的chrome调试服务，之后打开同一个实例，否则每次打开都是新的
+    options.add_argument("--remote-debugging-port=9444")
     return options
 
 
-def runWebdriver():
-    driver = webdriver.Chrome(chrome_options=getOptions())
-    driver.set_window_size(414, 896)
-    driver.implicitly_wait(4)
-    driver.get('https://www.zhipin.com/beijing/')
-    # initLogin(driver=driver)
-
 # TODO login的滑块验证开发成本太高
-def initLogin(driver):
+
+def init_login(driver):
     driver.find_element(
         By.XPATH, '//*[@id="wrap"]/div[1]/div[2]/a/div').click()
     time.sleep(3)
@@ -33,10 +30,37 @@ def initLogin(driver):
         By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[3]/span[2]/input').send_keys('13522142948')
     time.sleep(2)
 
-    driver.find_element(By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[5]/span/button').click()
+    driver.find_element(
+        By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[5]/span/button').click()
 
-    driver.find_element(By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[7]/input').click()
+    driver.find_element(
+        By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[2]/div[1]/form/div[7]/input').click()
+
+
+session_file = 'browser_session.data'
+
+def communicate(driver):
+  read_more = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/div[2]/div')
+  while not elementHasClass(read_more, 'disabled'):
+      driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+      time.sleep(2)
+  jd_list = driver.fined_element(By.XPATH, '//*[@id="main"]/div[3]/div[2]/ul')
+
+
+def elementHasClass(element, active):
+  class_str = element.get_attribute('class')
+  return active in class_str
+
+
+def create_driver():
+    driver = webdriver.Chrome(chrome_options=get_options())
+    driver.set_window_size(415, 1100)
+    driver.implicitly_wait(4)
+    time.sleep(2)
+    driver.get('https://www.zhipin.com/beijing/')
+    communicate(driver=driver)
+    return driver
 
 
 if __name__ == '__main__':
-    runWebdriver()
+    create_driver()

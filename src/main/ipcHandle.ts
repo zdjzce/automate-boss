9016d665702e5325ae54e35f3d83c1684e46e26c
zdjzce923 <493904954@qq.com>
2023-11-28 22:07:19 +0800

    feat: 操控 boss

--- src/main/ipcHandle.ts
+++ src/main/ipcHandle.ts
@@ -2,34 +2,38 @@ import { BrowserWindow } from 'electron'
 import path from 'path'
 import webdriver from 'selenium-webdriver'
 import { Builder } from 'selenium-webdriver'
-import Chrome from 'selenium-webdriver/chrome'
+import require from 'requirejs'
 
+const chrome = require('selenium-webdriver/chrome')
+const By = webdriver.By
+
+let userData = ''
 export const getUserDirName = async (app) => {
   const appDataPath = app.getPath('appData')
-
+  userData = path.join(appDataPath, 'Google/Chrome/Default')
   return path.join(appDataPath, 'Google/Chrome/Default')
 }
 
 export const createNewWindow = async () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadURL('https://www.zhipin.com/')
-
-  // 设置手机模式
-  const driver = await new Builder().forBrowser('chrome').build()
-
-  // options.setUserAgent(
-  //   'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
+  const chromeOptions = new chrome.Options()
+  chromeOptions.addArguments([
+    '--blink-settings=imagesEnabled=false',
+    'detach',
+    '--remote-debugging-port=9111',
+    '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    `user-data-dir=${userData}`
+  ])
+  chromeOptions.setMobileEmulation({ deviceName: 'iPhone XR' })
+  // chromeOptions.addArguments(
+  //   'user-data-dir=/private/var/folders/jz/hpmf14910h5d7sxh_3v42rxm0000gn/T/.com.google.Chrome.NPxjC5'
   // )
+  const driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build()
 
-  // const driver = new Builder().forBrowser('chrome').setChromeOptions(options).build()
-  await driver.get(win.webContents.getURL())
+  driver.get('https://www.zhipin.com/beijing/')
+  driver.manage().window().setRect({ width: 445, height: 1000 })
+
+  const read_more = driver.findElement(By.xpath('//*[@id="main"]/div[3]/div[2]/div'))
+  console.log('read_more:', read_more)
 }
 
 export const ipcEventHandle = {

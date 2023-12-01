0d2e095868a61e350546f85bc3fd9a4d15d10dbd
zdjzce923 <493904954@qq.com>
2023-12-01 12:16:41 +0800

    feat: 切换为 puppeteer

--- src/main/communicate.ts
+++ src/main/communicate.ts
@@ -1,85 +1,21 @@
 import { State } from '../renderer/src/state'
-const webdriver = require('selenium-webdriver')
-const puppeteer = require('puppeteer')
-const request_client = require('request-promise-native')
+import puppeteer from 'puppeteer-core'
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-const By = webdriver.By
-export async function communicate(driver, state: State) {
-  const reconmmandJobs = await driver
-    .findElement(By.xpath('/html/body/div[1]/div[1]/div[1]/div[3]/ul/li[2]/a'))
-    .click()
+export async function communicate(puppeteer, state: State) {
 
-  await sleep(1500)
-
-  const excepetJob = await driver
-    .findElement(By.xpath('//*[@id="wrap"]/div[2]/div[1]/div/div[1]/a[3]/span'))
-    .click()
-
-  // await scrollUlToEnd(driver, '//*[@id="wrap"]/div[2]/div[2]/div/div/div[1]/ul')
-  await getResponse()
 }
 
-export async function scrollUlToEnd(driver, xpath: string) {
-  let couldScroll = true
-  while (couldScroll) {
-    const element = await driver.findElement(By.xpath(xpath))
-    await driver.executeScript('arguments[0].scrollIntoView(true);', element)
-    await sleep(1500)
-
-    const response = await getResponse()
-    // for (const request of requests) {
-    //   if (request.method === 'GET') {
-    //     couldScroll = true
-    //   }
-    // }
-
-    // if () {
-    //   couldScroll = false
-    // }
-  }
-}
-
-const getResponse = async () => {
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
+const getResponse = async (page) => {
   const result: any[] = []
 
   await page.setRequestInterception(true)
 
   page.on('request', (request) => {
-    request_client({
-      uri: request.url(),
-      resolveWithFullResponse: true
-    })
-      .then((response) => {
-        const request_url = request.url()
-        const request_headers = request.headers()
-        const request_post_data = request.postData()
-        const response_headers = response.headers
-        const response_size = response_headers['content-length']
-        const response_body = response.body
 
-        result.push({
-          request_url,
-          request_headers,
-          request_post_data,
-          response_headers,
-          response_size,
-          response_body
-        })
-
-        console.log(result)
-        request.continue()
-      })
-      .catch((error) => {
-        console.error(error)
-        request.abort()
-      })
   })
 
-  await browser.close()
 }
 
 export async function elementHasClass(element, className) {
--- src/main/ipcHandle.ts
+++ src/main/ipcHandle.ts
@@ -1,13 +1,14 @@
-import require from 'requirejs'
 import { app } from 'electron'
 import { communicate } from './communicate'
 import { State } from '../renderer/src/state'
-
-const path = require('path')
-const webdriver = require('selenium-webdriver')
-const { Builder } = require('selenium-webdriver')
-const chrome = require('selenium-webdriver/chrome')
-
+import puppeteer from 'puppeteer-core';
+import path from 'path'
+/* 切换为 puppeteer。
+1. selenium 配合 node.js 没有简单的抓取网络请求的手段。 
+2. chrome-driver 体积有点大。 
+3. puppeteer 文档健全。
+4. puppeteer 为 Google 官方出品，而 selenium 的 chromedriver 更新不及时，导致较新版本不可用。
+*/
 let userData = ''
 export const getUserDirName = async (app) => {
   const appDataPath = app.getPath('appData')
@@ -21,35 +22,39 @@ export const updateState = async (event, ...args) => {
 }
 
 export const createNewWindow = async () => {
-  console.log('state:', state)
-  const chromeOptions = new chrome.Options()
-  chromeOptions.addArguments([
-    'detach',
-    '--remote-debugging-port=9111',
-    `user-data-dir=${userData}`
-  ])
+  const exPath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  const browser = await puppeteer.launch({
+    executablePath: exPath,
+    userDataDir: userData,
+    headless: false
+  })
 
-  const serviceBuilder = new chrome.ServiceBuilder(
-    path.join(
-      // process.resourcesPath,
-      app.getAppPath(),
-      // 'app.asar.unpacked',
-      'node_modules',
-      'electron-chromedriver',
-      'bin',
-      'chromedriver'
-    )
-  )
-  const driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(chromeOptions)
-    .setChromeService(serviceBuilder)
-    .build()
+  const page = await browser.newPage();
+  page.setViewport({
+    width: 1280,
+    height: 720,
+    deviceScaleFactor: 0.5,
+  })
+  await page.goto('http://test.mihuashi.com/dashboard/projects')
+  await setTimeout(() => { }, 1000)
 
-  driver.get('https://www.zhipin.com/')
-  driver.manage().window().setRect({ width: 1280, height: 720 })
 
-  communicate(driver, state)
+  await page.waitForXPath('//*[@id="main-container"]/section/main/section/article/div/div[1]/section[2]/section[2]/a/button', { timeout: 1000 });
+
+  const element = await page.waitForXPath('//*[@id="main-container"]/section/main/section/article/div/div[1]/section[2]/section[2]/a/button')
+  element?.click()
+  // await element[0].click()
+
+  // communicate(1, state)
+
+  page.on("response", async (response) => {
+
+    if (response.url().includes("custom_shortcuts")) {
+        console.log(await response.text());
+    }
+
+  })
+
 }
 
 export const ipcEventHandle = {
